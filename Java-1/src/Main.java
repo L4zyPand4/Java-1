27ebb57eced3758a3319bdfae7cfd907cf3a977a
L4zyPand4 <79875716+L4zyPand4@users.noreply.github.com>
2023-02-10 15:29:42 +0330

    Update1
    added some conditions and whatnot

--- Java-1/src/Main.java
+++ Java-1/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("How old are you?");
+        Scanner INPUT = new Scanner(System.in);
+        int age = INPUT.nextInt();
+
+        if (age < 18) { //ilegal
+            System.out.println("You do not meet the age requirment.");
+            return;
+        }
+
+        else { //legal
+            System.out.println("Welcome!");
+        }
+
+        System.out.println("What is your name?");
+        String name = INPUT.nextLine();
+        if(name == INPUT.nextLine()){
+            System.out.println("Hello," + name);
+        }
+
+        else{
+            System.out.println("Invalid name.");
+            return;
+        }
+
+
+
+
     }
 }
