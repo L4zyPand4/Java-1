c8344663ffcc62e61169663b547ba3cdd5560386
L4zyPand4 <79875716+L4zyPand4@users.noreply.github.com>
2023-02-14 23:03:25 +0330

    V.3
    Fixed bugs & cool stuff

--- Java-1/src/Main.java
+++ Java-1/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import org.bukkit.entity;
 public class Main {
     public static void main(String[] args) {
 
@@ -15,10 +16,10 @@ public class Main {
             System.out.println("Welcome!");
         }
 
-        System.out.println("What is your name? (Without spaces) ");
+        System.out.println("What is your first name?");
         String name = INPUT.next();
            if(name != null){
-            System.out.println("Name confirmed.");
+            System.out.println(" ");
            }
 
            else{
@@ -26,21 +27,34 @@ public class Main {
             return;
            }
 
+
+        System.out.println("What is your last name?");
+        String lastname = INPUT.next();
+        if(lastname != null){
+            System.out.println("Name confirmed.");
+        }
+
+        else{
+            System.out.println("ERROR");
+            return;
+        }
+
         System.out.println("Where are you from?");
            String country = INPUT.next();
            if(country.equalsIgnoreCase("Iran")) {
-              System.out.println("سلام" + " " + name);
+              System.out.println("سلام" + " " + name +" "+ lastname);
            }
 
            else if(country.equalsIgnoreCase("Canada")){
-               System.out.println("Hello" + " " + name);
+               System.out.println("Hello" + " " + name +" "+ lastname);
            }
            else if (country.equalsIgnoreCase("Germany")) {
-               System.out.println("Hallo" + " " + name);
+               System.out.println("Hallo" + " " + name +" "+ lastname);
            }
 
            else{
                System.out.println("Unknown region.");
            }
+           
     }
 }
